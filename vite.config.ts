a199535947b5bc0bb71f4991b053e1cc0a1cf622
amlan01 <amlan.random@gmail.com>
2026-05-26 16:50:31 +0530

    configs updated

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/homepage/',
+  base: '/bio/',
   plugins: [react()],
-  build: {
-    outDir: 'build',
-  },
 })
